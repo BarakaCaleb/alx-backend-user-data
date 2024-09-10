e0ac2bc213d4cc89ef03837dcf0b8100b3c76818
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-10 19:07:48 +0300

    Task 0

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+"""_summary_
+"""
 
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
 class User(Base):
+    """_summary_
 
+    Args:
+        Base (_type_): _description_
+    """
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
