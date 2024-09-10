2725858280fab1d30e177adabcb9c377744ff404
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-10 13:00:49 +0300

    changes

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,7 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
+
+
 class User(Base):
+
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String, nullable=False)
