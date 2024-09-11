33621b0b9677d6ad824a5dc7ed8735fa229a59b4
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-11 16:58:47 +0300

    Debugging

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,3 +16,23 @@ def _hash_password(self, password: str) -> str:
     Convert password to hash
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> Union[None, User]:
+        """"
+        Register a user with email and password and return User object
+        """
+        try:
+            # Find the user by the provided email
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            raise ValueError('User {} already exists'.format(email))
