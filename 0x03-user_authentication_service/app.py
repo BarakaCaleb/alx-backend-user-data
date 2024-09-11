3367c977b83aa17aaf8eabe19f831b719ac47f96
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-11 18:02:20 +0300

    flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index() -> str:
+    """GET route that returns a JSON response."""
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
