c09bc6d4db8eec9a92a442e3cebd2f73a3d62241
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-12 13:39:03 +0300

    logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,6 +48,16 @@ def login() -> str:
 
     return response
 
+@app.route('/sessions', method=['DELETE'])
+def logout() -> str:
+    """
+    Logout functionality"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if not user:
+        abort(403)
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
