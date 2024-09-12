3506a75709bb49c13149903200092243731661e3
BarakaCaleb <calebbaraka79@gmail.com>
2024-09-12 15:05:27 +0300

    reset password

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -87,6 +87,7 @@ def get_reset_password_token() -> str:
     except Exception:
         abort(403)
 
+
 @app.route('/reset_password', methods=['PUT'])
 def update_password() -> str:
     """_summary_
